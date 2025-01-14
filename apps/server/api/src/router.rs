bd97660fa7e6956f820801a30f9200e08fe866cf
sripwoud <me@sripwoud.xyz>
2025-01-14 11:05:46 +0100

    feat: define separate `bindings` bin to generate ts types for client

--- apps/server/api/src/router.rs
+++ apps/server/api/src/router.rs
@@ -1,14 +1,10 @@
+use crate::user_router;
 use crate::Context;
-use crate::{user::dtos::UserResponseDto, user_router};
-use domain::Read;
-use rspc::{Config, Router};
-use std::{path::PathBuf, sync::Arc};
+use rspc::Router;
+use std::sync::Arc;
 
 pub fn mount() -> Arc<Router<Context>> {
     Router::<Context>::new()
-        .config(Config::new().export_ts_bindings(
-            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../../client/src/lib/bindings.ts"),
-        ))
         .merge("users.", user_router())
         .build()
         .arced()
--- prisma/src/lib.rs
+++ prisma/src/lib.rs
@@ -3,5 +3,8 @@ pub use generated::*;
 use prisma_client_rust::NewClientError;
 
 pub async fn init_prisma() -> Result<PrismaClient, NewClientError> {
-    PrismaClient::_builder().build().await
+    PrismaClient::_builder()
+        .with_url(std::env::var("DATABASE_URL").unwrap())
+        .build()
+        .await
 }
