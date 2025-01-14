73581786721882583555c6e769a7ddb097a5422f
sripwoud <me@sripwoud.xyz>
2025-01-14 07:48:19 +0100

    fix(server): setup cors

--- apps/server/bin/src/main.rs
+++ apps/server/bin/src/main.rs
@@ -1,4 +1,5 @@
 use api::{mount, Context};
+use axum::http::Method;
 use axum::{routing::get, Router};
 use dotenvy::dotenv;
 use infra::UserRepository;
@@ -8,6 +9,7 @@ use std::env;
 use std::path::PathBuf;
 use std::sync::Arc;
 use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
@@ -15,6 +17,9 @@ async fn main() {
     let prisma = init_prisma().await.unwrap();
     let user_service = Arc::new(UserService::new(Arc::new(UserRepository::new(prisma))));
     let router = mount();
+    let cors = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST])
+        .allow_origin(Any);
     let app = Router::new()
         .route("/", get(|| async { "Hello rspc!" }))
         .nest(
@@ -22,7 +27,8 @@ async fn main() {
             rspc_axum::endpoint(router, move || Context {
                 user_service: user_service.clone(),
             }),
-        );
+        )
+        .layer(cors);
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     println!("Server running on 0.0.0.0:3000");
