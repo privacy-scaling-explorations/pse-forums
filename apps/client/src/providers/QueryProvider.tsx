933bc7aecdc704fe59f826b2c5966add1e7ef2b2
sripwoud <me@sripwoud.xyz>
2025-01-14 09:58:01 +0100

    chore: refactor cargo workspace deps

--- apps/client/src/providers/QueryProvider.tsx
+++ apps/client/src/providers/QueryProvider.tsx
@@ -1,24 +1,24 @@
-import { createClient, FetchTransport } from "@rspc/client";
-import { QueryClient } from "@tanstack/react-query";
-import { Procedures } from "l/bindings";
-import { rspc } from "l/rspc";
-import { type JSXElementConstructor, type ReactElement, useState } from "react";
+import { createClient, FetchTransport } from "@rspc/client"
+import { QueryClient } from "@tanstack/react-query"
+import { Procedures } from "l/bindings"
+import { rspc } from "l/rspc"
+import { type JSXElementConstructor, type ReactElement, useState } from "react"
 
 export function QueryProvider({
   children,
 }: {
-  children: ReactElement<any, string | JSXElementConstructor<any>> | undefined;
+  children: ReactElement<any, string | JSXElementConstructor<any>> | undefined
 }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient())
   const [rspcClient] = useState(() =>
     createClient<Procedures>({
       transport: new FetchTransport("http://localhost:3000/rspc"),
-    }),
-  );
+    })
+  )
 
   return (
     <rspc.Provider client={rspcClient} queryClient={queryClient}>
       {children}
     </rspc.Provider>
-  );
+  )
 }
