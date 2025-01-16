8023bdfe002931f2d09147d6116a753aa58c3dfb
jacque006 <jacque006@users.noreply.github.com>
2025-01-15 18:28:11 -0700

    chore: Add comment migration

--- apps/server/src/controller/migration.rs
+++ apps/server/src/controller/migration.rs
@@ -1,16 +1,17 @@
 use std::{collections::HashMap, path::PathBuf};
 
-use bincode::{Decode, Encode};
+use bincode::{config::standard, Decode, Encode};
 use serde::Serialize;
-use sled::{Batch, Db};
+use sled::{transaction::ConflictableTransactionError, Batch, Db, Transactional};
 use tracing::info;
 
 use crate::{
     controller::{
-        db_utils::{get_batch, get_count, incr_id, u32_to_ivec},
+        db_utils::{get_batch, get_count, incr_id, set_one_with_key, u32_to_ivec},
         inn::inn_add_index,
         meta_handler::ParamsPage,
-        Inn, Post, User,
+        user::InnRole,
+        Comment, Inn, Post, User,
     },
     get_one, ivec_to_u32, set_one, AppError,
 };
@@ -23,14 +24,37 @@ struct Migration {
     complete: bool,
 }
 
+// Copied from inn.rs
+fn inn_rm_index(db: &Db, iid: u32, pid: u32) -> Result<u8, AppError> {
+    let tl_idx_tree = db.open_tree("post_timeline_idx")?;
+    let tl_tree = db.open_tree("post_timeline")?;
+
+    (&tl_idx_tree, &tl_tree)
+        .transaction(|(tl_idx, tl)| {
+            let mut inn_type = 0;
+
+            let k = [&u32_to_ivec(iid), &u32_to_ivec(pid)].concat();
+            if let Some(v) = tl_idx.remove(&*k)? {
+                let k = [&v[0..4], &u32_to_ivec(iid), &u32_to_ivec(pid)].concat();
+                if let Some(v) = tl.remove(k)? {
+                    inn_type = v[0];
+                }
+            }
+
+            Ok::<u8, ConflictableTransactionError<AppError>>(inn_type)
+        })
+        .map_err(|e| AppError::Custom(format!("transaction error: {e}")))
+}
+
 /// Migrates data from another Freedit instance to this one.
-/// Note that this migration is not atomic.
+/// Note that this migration is not atomic. It also assumes that
+/// the destination freedit has at least one inn that
+/// posts & comments will be migrated to.
 ///
 /// Includes:
-/// - Default Inn
 /// - Users
+/// - TODO Inn Role?
 /// - Posts
-/// - Solos (TODO May not need)
 /// - Comments
 ///
 pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool, AppError> {
@@ -47,7 +71,9 @@ pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool,
         return Ok(true);
     }
 
-    // Migrate Inn (Use existing)
+    //////////////////////////////////
+    //  Migrate Inn (Use existing) //
+    //////////////////////////////////
     let num_incoming_inns = get_count(&m_db, "default", "inns_count")?;
     info!("processing {0} incoming Inns", num_incoming_inns);
 
@@ -59,11 +85,13 @@ pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool,
             "more than 1 incoming or current inn(s) found".to_string(),
         ));
     }
-    let iid = 0; // TODO Check this is correct defaul inn id
+    let iid = 1; // First inn
     let iid_ivec = u32_to_ivec(iid);
     let inn: Inn = get_one(db, "inns", iid)?;
 
-    // Migrate User
+    //////////////////
+    // Migrate User //
+    //////////////////
     let num_incoming_users = get_count(&m_db, "default", "users_count")?;
     info!("processing {0} incoming Users", num_incoming_users);
 
@@ -110,7 +138,9 @@ pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool,
         }
     }
 
-    // Migrate Post
+    //////////////////
+    // Migrate Post //
+    //////////////////
     let num_incoming_posts = get_count(&m_db, "default", "posts_count")?;
     info!("processing {0} incoming Posts", num_incoming_posts);
 
@@ -132,6 +162,7 @@ pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool,
             return Err(AppError::Custom(msg));
         }
         let uid = uid_opt.unwrap().clone();
+        let uid_ivec = u32_to_ivec(uid);
 
         let pid = incr_id(db, "posts_count")?;
         let pid_ivec = u32_to_ivec(pid);
@@ -163,7 +194,7 @@ pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool,
             db.open_tree("tan")?.insert(format!("post{}", pid), &[])?;
         }
 
-        // Update other indicies & stats
+        // Update other indices & stats
         let k = [&iid_ivec, &pid_ivec].concat();
         db.open_tree("inn_posts")?.insert(k, &[])?;
 
@@ -174,22 +205,87 @@ pub fn migrate_data_from_freedit(db: &Db, migration_db: PathBuf) -> Result<bool,
 
         inn_add_index(db, iid, pid, new_post.created_at as u32, inn.inn_type)?;
         User::update_stats(db, uid, "post")?;
+
+        //////////////////////////////
+        // Migrate Comments on Post //
+        //////////////////////////////
+        let incoming_pid = i_p.pid;
+        let incoming_pid_ivec = u32_to_ivec(incoming_pid);
+
+        let num_incoming_comments = get_count(&m_db, "post_comments_count", &pid_ivec)?;
+        info!(
+            "processing {0} incoming Comments for pid {1}, incoming pid {2}",
+            num_incoming_comments, pid, incoming_pid
+        );
+
+        let incoming_post_comments_tree = &m_db.open_tree("post_comments")?;
+        let post_comments_count_tree = &db.open_tree("post_comments_count")?;
+        let first_comment_id: usize = 1;
+
+        for i in first_comment_id..=num_incoming_comments {
+            // Lookup incoming comment
+            let incoming_k = [&incoming_pid_ivec, &u32_to_ivec(i as u32)].concat();
+            let incoming_v_opt = &incoming_post_comments_tree.get(incoming_k)?;
+            if incoming_v_opt.is_none() {
+                let msg = format!(
+                    "comment vector for i {0}, incoming pid {1} not found",
+                    i, incoming_pid
+                );
+                return Err(AppError::Custom(msg));
+            }
+            let incoming_v = incoming_v_opt.clone().unwrap();
+            let (comment, _): (Comment, usize) =
+                bincode::decode_from_slice(&incoming_v, standard())?;
+
+            // Create new comment
+            // This id should match the incoming one, but we still need to update current db
+            let cid = incr_id(post_comments_count_tree, &pid_ivec)?;
+            if cid != comment.cid {
+                let msg = format!(
+                    "comment ids do not match. incoming cid {0}, new cid {1}",
+                    comment.cid, cid
+                );
+                return Err(AppError::Custom(msg));
+            }
+            let cid_ivec = u32_to_ivec(cid);
+
+            info!("creating comment. cid {0}", cid);
+
+            let new_comment = Comment {
+                cid: cid,
+                pid: pid, // Use new pid
+                uid: uid,
+                reply_to: comment.reply_to,
+                content: comment.content,
+                created_at: comment.created_at,
+                is_hidden: comment.is_hidden,
+            };
+
+            let k = [&pid_ivec, &cid_ivec].concat();
+            set_one_with_key(db, "post_comments", k, &new_comment)?;
+
+            let k = [&uid_ivec, &pid_ivec, &cid_ivec].concat();
+            db.open_tree("user_comments")?.insert(k, &[])?;
+
+            // only the fellow could update the timeline by adding comment
+            let inn_role = InnRole::get(db, iid, uid)?;
+            if inn_role >= Some(InnRole::Fellow) {
+                let inn_type = inn_rm_index(db, iid, pid)?;
+                inn_add_index(db, iid, pid, new_comment.created_at as u32, inn_type)?;
+            }
+
+            User::update_stats(db, uid, "comment")?;
+
+            if inn.is_open_access() {
+                db.open_tree("tan")?
+                    .insert(format!("comt{}/{}", pid, cid), &[])?;
+            }
+        }
     }
 
-    // Migrate Solo
-    // This may be unnecessary to implement if there are no solo posts
-    let num_incoming_solos = get_count(&m_db, "default", "solos_count")?;
-    info!("processing {0} incoming Solos", num_incoming_solos);
-
-    // Migrate Comment
-    // Note that these are attached to a post/solo.
-    // let num_incoming_users = get_count(&m_db, "post_comments_count", pid)?;
-    // info!("processing {0} incoming Comments", num_incoming_users);
-
-    // Insert migration completion.
-    // Check to see if we've already brought in incoming db
-    let migration = Migration { complete: true };
-    set_one(&m_db, MIGRATION_TREE, MIGRATION_ID, &migration)?;
+    // TODO Insert migration completion.
+    // let migration = Migration { complete: true };
+    // set_one(&m_db, MIGRATION_TREE, MIGRATION_ID, &migration)?;
 
     info!("migration complete");
     Ok(true)
