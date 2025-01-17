abc0da4298e4a6e7322c8e3772c0428b7b9f6cde
sripwoud <me@sripwoud.xyz>
2025-01-17 09:55:06 +0100

    chore: use `bin` feature flag for prisma bin

--- db/prisma/src/lib.rs
+++ db/prisma/src/lib.rs
@@ -1,7 +1,11 @@
+#[cfg(not(feature = "bin"))]
 mod generated;
+#[cfg(not(feature = "bin"))]
 pub use generated::*;
+#[cfg(not(feature = "bin"))]
 use prisma_client_rust::NewClientError;
 
+#[cfg(not(feature = "bin"))]
 pub async fn init_prisma() -> Result<PrismaClient, NewClientError> {
     PrismaClient::_builder()
         .with_url(std::env::var("DATABASE_URL").unwrap())
