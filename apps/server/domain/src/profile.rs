54ecc7bc38d4fbfefa15622c5a99e28c139c6737
sripwoud <me@sripwoud.xyz>
2025-01-29 22:55:20 +0100

    refactor: use `Username` in `User`, `Claim`, `Profile`

--- apps/server/domain/src/profile.rs
+++ apps/server/domain/src/profile.rs
@@ -1,12 +1,22 @@
+use crate::Username;
 use chrono::{DateTime, FixedOffset};
-use struct_convert::Convert;
 
-#[derive(Convert)]
-#[convert(from = "db::profile::Data")]
 pub struct Profile {
     pub id: i32,
     pub about: Option<String>,
     pub created_at: DateTime<FixedOffset>,
-    pub username: String,
+    pub username: Username,
     pub url: Option<String>,
+}
+
+impl From<db::profile::Data> for Profile {
+    fn from(data: db::profile::Data) -> Self {
+        Self {
+            id: data.id,
+            about: data.about,
+            created_at: data.created_at,
+            username: data.username.try_into().unwrap(), // if was inserted successfully in DB it is valid, safe to unwrap
+            url: data.url,
+        }
+    }
 }
--- apps/server/domain/src/user.rs
+++ apps/server/domain/src/user.rs
@@ -1,13 +1,24 @@
+use crate::{Email, Username};
 use chrono::{DateTime, FixedOffset};
-use struct_convert::Convert;
 
-#[derive(Convert)]
-#[convert(from = "db::user::Data")]
 pub struct User {
     pub id: i32,
     pub created_at: DateTime<FixedOffset>,
-    pub email: String,
+    pub email: Email,
     pub encrypted_password: String,
     pub salt: String,
-    pub username: String,
+    pub username: Username,
+}
+
+impl From<db::user::Data> for User {
+    fn from(data: db::user::Data) -> Self {
+        Self {
+            id: data.id,
+            created_at: data.created_at,
+            email: data.email.try_into().unwrap(), // if was inserted successfully in DB it is valid, safe to unwrap
+            encrypted_password: data.encrypted_password,
+            salt: data.salt,
+            username: data.username.try_into().unwrap(), // if was inserted successfully in DB it is valid, safe to unwrap
+        }
+    }
 }
