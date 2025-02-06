7f743f0aab58f2ff71f158e446ece687478ad41d
sripwoud <me@sripwoud.xyz>
2025-02-06 14:48:09 +0100

    fix revive storage function

--- apps/client/src/components/forms/Settings/index.tsx
+++ apps/client/src/components/forms/Settings/index.tsx
@@ -9,15 +9,17 @@ import { RecoveryCode } from "./_RecoveryCode"
 export function Settings() {
   const { auth } = useAuth()
   const { data, error, isLoading } = useQuery(
-    // @ts-ignore TODO: fix, what need to get inner?
-    ["profile.read", auth.mapOrSync("", ({ username }) => username.inner)],
+    // @ts-ignore
+    ["profile.read", auth.inner?.username],
     {
       enabled: auth.isSome(),
     },
   )
 
   if (error) return <div>Error: {error.message}</div>
-  if (data === undefined || isLoading) return <div>Loading...</div>
+  if (data === undefined || isLoading || auth.isNone()) {
+    return <div>Loading...</div>
+  }
   return (
     <div className="space-y-8">
       <h1 className="text-xl font-bold">Settings</h1>
