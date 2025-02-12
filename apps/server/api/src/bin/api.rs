534d2bff0316f0a8ea38bcd4b044f125d5b9b6a4
sripwoud <me@sripwoud.xyz>
2025-02-12 23:04:49 +0100

    feat: add grafana/loki logs monitoring (#94)

--- apps/server/api/src/bin/api.rs
+++ apps/server/api/src/bin/api.rs
@@ -1,13 +1,38 @@
+use anyhow::{Context, Result};
+use std::env;
+use std::process;
 use tokio::net::TcpListener;
 use tower_http::cors::CorsLayer;
+use tracing::info;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
+use url::Url;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    let (loki_layer, loki_task) = tracing_loki::builder()
+        .label("host", "pse-forum-server")? // Add a label to distinguish logs
+        .extra_field("pid", format!("{}", process::id()))? // Include process ID
+        .build_url(Url::parse(&env::var("LOKI_URL").unwrap()).unwrap())?;
+    tracing_subscriber::registry()
+        .with(fmt::layer()) // log to stdout
+        .with(loki_layer) // send logs to loki
+        .init();
+
+    info!("Starting loki task");
+    tokio::spawn(loki_task);
+    info!("Starting server");
+
     // TODO: define more restrictive CORS policy https://github.com/privacy-scaling-explorations/pse-forums/issues/21
     let app = api::app().await.layer(CorsLayer::very_permissive());
     // TODO: read port from env
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000")
+        .await
+        .context("Failed to bind to port")?;
 
     println!("Server running on 0.0.0.0:3000");
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .context("Failed to serve")?;
+
+    Ok(())
 }
