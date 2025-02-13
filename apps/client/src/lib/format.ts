13ccece0f205beab45668c84cf459f56312cc649
sripwoud <me@sripwoud.xyz>
2025-02-13 22:47:31 +0100

    feat: preview markdown and latex (#99)

--- apps/client/src/lib/format.ts
+++ apps/client/src/lib/format.ts
@@ -1,3 +1,21 @@
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export enum Format {
+  Markdown = "markdown",
+  Latex = "latex",
+  Raw = "raw",
+}
+
+export function detectFormat(text: string) {
+  if (/\$\$[\s\S]*?\$\$/.test(text) || /\$[^$]+\$/.test(text)) {
+    return Format.Latex
+  }
+
+  if (/(^#{1,6} |\*\*|__|`{1,3}|\[.*?\]\(.*?\)|\*|-)/m.test(text)) {
+    return Format.Markdown
+  }
+
+  return Format.Raw
+}
