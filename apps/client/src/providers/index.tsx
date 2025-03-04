f59c0acac543b2259fc1cd83b8fb19b43112be29
Kalidou Diagne <kld.diagne@gmail.com>
2025-03-04 22:06:57 +0100

    fix: tooltip

--- apps/client/src/providers/index.tsx
+++ apps/client/src/providers/index.tsx
@@ -1,16 +1,16 @@
 import { GlobalProvider } from "@/contexts/GlobalContext";
 import { QueryProvider } from "./QueryProvider";
 import { RouterProvider } from "./RouterProvider";
-import { TooltipProvider } from "@/components/ui/Tooltip";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 
 export function Providers() {
   return (
     <QueryProvider>
-      <TooltipProvider delayDuration={0}>
+      <TooltipPrimitive.Provider delayDuration={0}>
         <GlobalProvider>
           <RouterProvider />
         </GlobalProvider>
-      </TooltipProvider>
+      </TooltipPrimitive.Provider>
     </QueryProvider>
   );
 }
