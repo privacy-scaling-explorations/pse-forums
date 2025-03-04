06d58e1588d4e54c89d8dad9e09ea162722cae95
Kalidou Diagne <kld.diagne@gmail.com>
2025-03-04 21:48:42 +0100

    feat: fix import

--- apps/client/src/providers/index.tsx
+++ apps/client/src/providers/index.tsx
@@ -1,7 +1,7 @@
 import { GlobalProvider } from "@/contexts/GlobalContext";
 import { QueryProvider } from "./QueryProvider";
 import { RouterProvider } from "./RouterProvider";
-import { TooltipProvider } from "@/components/ui/Tooltip";
+import { TooltipProvider } from "./../components/ui/Tooltip";
 
 export function Providers() {
   return (
