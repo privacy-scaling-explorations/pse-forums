04a8826c04a6432e293009d236b8eeedde038e19
Kalidou Diagne <kld.diagne@gmail.com>
2025-03-05 17:07:13 +0100

    feat: fe progress

--- apps/client/src/sections/App/index.tsx
+++ apps/client/src/sections/App/index.tsx
@@ -1,11 +1,11 @@
 import { PageContent } from "@/components/PageContent";
-import { Link, PlusIcon } from "lucide-react";
+import { PlusIcon } from "lucide-react";
 import { postMocks } from "mocks/postMocks";
 import { Badge } from "@/components/ui/Badge";
 import { Button } from "@/components/ui/Button";
 import { PostAuthor } from "@/sections/Post/PostAuthor";
 import { PostCard } from "@/sections/Post/PostCard";
-
+import { Link } from "@tanstack/react-router";
 export const HomePage = () => {
   return (
     <PageContent className="flex flex-col gap-6">
